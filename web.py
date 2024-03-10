c95695338a136f337de70bc165c275127be2928a
Utn2 <biatzz@gmail.com>
2024-03-10 12:24:45 +0100

    Inital commit

--- web.py
+++ web.py
@@ -9,7 +9,7 @@ def add_todo():
     functions.write_todos(todos)
 
 
-st.title("My Todo App 2")
+st.title("My Todo App")
 st.subheader("This is my todo app.")
 st.write("This app is to increase your productivity.")
 
