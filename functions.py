b4b637e9d8ad35f6a6f55b6bdce834b6f37d9144
Utn2 <biatzz@gmail.com>
2024-03-10 12:13:34 +0100

    Title change

---
+++ functions.py
@@ -0,0 +1,19 @@
+# Custom Function Definition
+
+import os
+# def get_todos(filepath="/app/mytodo/todos.txt"):
+def get_todos(filepath=r"C:\Users\Utente\Python\IdeaProjects\CourseNew\Files\todos.txt"):
+# def get_todos(filepath=os.path.abspath(r"Files\todos.txt")):
+   # os.path.abspath("todos.txt")
+    with open(filepath, 'r') as file:
+        todos_local = file.readlines()
+    return todos_local
+
+# def write_todos(todos_arg, filepath=os.path.abspath(r"Files\todos.txt")):
+# def write_todos(todos_arg, filepath="/app/mytodo/todos.txt"):
+def write_todos(todos_arg, filepath=r"C:\Users\Utente\Python\IdeaProjects\CourseNew\Files\todos.txt"):
+    with open(filepath, 'w') as file:
+        todos = file.writelines(todos_arg)
+
+if __name__ == "__C2_TodoList_Latest__":
+    print("Hello")
