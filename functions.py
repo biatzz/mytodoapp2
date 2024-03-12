3cffae903bec3b78cc2692108e8d505c9315271d
biatzz <149577210+biatzz@users.noreply.github.com>
2024-03-12 18:29:37 +0100

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,8 +1,9 @@
 # Custom Function Definition
 
 import os
+def get_todos(filepath=r"/todos.txt"):
 # def get_todos(filepath="/app/mytodo/todos.txt"):
-def get_todos(filepath=r"/app/mytodoapp2/todos.txt"):
+# def get_todos(filepath=r"/app/mytodoapp2/todos.txt"):
 # def get_todos(filepath=os.path.abspath(r"Files\todos.txt")):
    # os.path.abspath("todos.txt")
     with open(filepath, 'r') as file:
@@ -11,7 +12,8 @@ def get_todos(filepath=r"/app/mytodoapp2/todos.txt"):
 
 # def write_todos(todos_arg, filepath=os.path.abspath(r"Files\todos.txt")):
 # def write_todos(todos_arg, filepath="/app/mytodo/todos.txt"):
-def write_todos(todos_arg, filepath=r"/app/mytodoapp2/todos.txt"):
+# def write_todos(todos_arg, filepath=r"/app/mytodoapp2/todos.txt"):
+def write_todos(todos_arg, filepath=r"/todos.txt"):   
     with open(filepath, 'w') as file:
         todos = file.writelines(todos_arg)
 
